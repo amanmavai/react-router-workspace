689fc4d9e16492122ebe70e07c9403624d9b8cdb
Aman Mavai <amanmavai@gmail.com>
2022-11-13 09:01:28 +0530

    add support for edit contact route

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import {createBrowserRouter, RouterProvider, Route} from "react-router-dom";
 import ErrorPage from "./error-page";
 import "./index.css";
 import Root, {loader as rootLoader, action as rootAction} from "./routes/root";
-import Contact from "./routes/contact";
+import Contact, {loader as contactLoader} from "./routes/contact";
+import EditContact from "./routes/edit";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +18,12 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
       },
     ],
   },
