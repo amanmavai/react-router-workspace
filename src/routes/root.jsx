47ee982d0b776cd75eb1893a06929988f0cb2e92
Aman Mavai <amanmavai@gmail.com>
2022-11-13 14:20:58 +0530

    use useSubmit

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Form, Outlet, useLoaderData, redirect, NavLink, useNavigation} from "react-router-dom";
+import {Form, Outlet, useLoaderData, redirect, NavLink, useNavigation, useSubmit} from "react-router-dom";
 import {createContact, getContacts} from "../contacts";
 
 export async function action() {
@@ -17,6 +17,7 @@ export async function loader({request}) {
 export default function Root() {
   const {contacts, q} = useLoaderData();
   const navigation = useNavigation();
+  const submit = useSubmit();
 
   React.useEffect(() => {
     document.getElementById("q").value = q;
@@ -28,7 +29,17 @@ export default function Root() {
         <h1>React Router Contacts</h1>
         <div>
           <Form id="search-form" role="search">
-            <input id="q" aria-label="Search contacts" placeholder="Search" type="search" name="q" defaultValue={q} />
+            <input
+              id="q"
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+              defaultValue={q}
+              onChange={(event) => {
+                submit(event.currentTarget.form);
+              }}
+            />
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
           </Form>
